eb784a5543d326ce221e0c663cb6c1fd56d99ee4
Iann Oliveira <ianncosta13@gmail.com>
2023-08-24 14:20:03 -0300

    Criação do Login e Password junto com as credenciais para autenticação

---
+++ app/Http/Controllers/Me/MeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Me;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\User\UserResource;
+
+class MeController extends Controller
+{
+    public function show() {
+        return new UserResource(auth()->user());
+    }
+}
